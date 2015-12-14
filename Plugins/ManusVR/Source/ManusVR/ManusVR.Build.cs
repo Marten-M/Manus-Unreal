a30af65601f3f137691a614e8f00afc36aab159a
André van Schoubroeck <andre@philosopher.it>
2015-12-14 16:03:27 +0100

    Copying Manus.dll to Plugin and Release directories

--- Plugins/ManusVR/Source/ManusVR/ManusVR.Build.cs
+++ Plugins/ManusVR/Source/ManusVR/ManusVR.Build.cs
@@ -32,7 +32,7 @@ public class ManusVR : ModuleRules
 
         PublicIncludePaths.AddRange(
             new string[] {
-					"ManusVT/Public",
+					"ManusVR/Public",
 					// ... add public include paths required here ...
 				}
             );
@@ -79,6 +79,26 @@ public class ManusVR : ModuleRules
             );
         LoadManusLib(Target);
     }
+
+    // https://github.com/fmod/ue4integration/blob/master/FMODStudio/Source/FMODStudio/FMODStudio.Build.cs
+    private void CopyFile(string source, string dest)
+    {
+        //System.Console.WriteLine("Copying {0} to {1}", source, dest);
+        if (System.IO.File.Exists(dest))
+        {
+            System.IO.File.SetAttributes(dest, System.IO.File.GetAttributes(dest) & ~System.IO.FileAttributes.ReadOnly);
+        }
+        try
+        {
+            System.IO.File.Copy(source, dest, true);
+        }
+        catch (System.Exception ex)
+        {
+            System.Console.WriteLine("Failed to copy file: {0}", ex.Message);
+        }
+    }
+
+
     public bool LoadManusLib(TargetInfo Target)
     {
         bool isLibrarySupported = false;
@@ -90,7 +110,30 @@ public class ManusVR : ModuleRules
             string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "Win64" : "Win32";
 
             PublicAdditionalLibraries.Add(Path.Combine(LibraryPath, PlatformString, "Manus.lib"));
-            RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(LibraryPath, PlatformString, "Manus.dll")));
+            //RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(LibraryPath, PlatformString, "Manus.dll"))); 
+
+            // Trying to specify the DLL as Runtime Dependency has no effect.
+            // Copying the DLL to the target directory by code
+
+            string From = Path.Combine(LibraryPath, PlatformString, "Manus.dll");
+            string destPathEditor = (ModulePath + "/../../Binaries/" + PlatformString);
+            string destPathRelease = (ModulePath + "/../../../../Binaries/" + PlatformString);
+            System.Console.WriteLine(ModulePath + " destPathEditor = " + destPathEditor);
+            System.Console.WriteLine(ModulePath + " destPathRelease = " + destPathRelease);
+
+
+            System.IO.Directory.CreateDirectory(destPathEditor);
+            System.IO.Directory.CreateDirectory(destPathRelease);
+            destPathEditor += "/Manus.dll";
+            destPathRelease += "/Manus.dll";
+
+            System.Console.WriteLine("Editor: Origin =" + From + " Destination = " + destPathEditor);
+            System.Console.WriteLine("Game:   Origin =" + From + " Destination = " + destPathRelease);
+
+            CopyFile(From, destPathEditor);
+            CopyFile(From, destPathRelease);
+
+
         }
 
 
