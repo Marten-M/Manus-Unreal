6bc1c6ef8a90aa31e19d4c4fd58acc3e5dd9a747
Heiko Fink aka Cyx <hfink@web.de>
2016-04-16 17:52:44 +0200

    Add support for UE 4.11

--- Plugins/ManusVR/Source/ManusVR/Public/ManusVRAnimInstance.h
+++ Plugins/ManusVR/Source/ManusVR/Public/ManusVRAnimInstance.h
@@ -3,16 +3,40 @@
 
 #pragma once
 #include "ManusLib.h"
+#include "Runtime/Launch/Resources/Version.h"
+#if (ENGINE_MAJOR_VERSION >= 4) && (ENGINE_MINOR_VERSION >= 11)
+#include "Animation/AnimInstanceProxy.h"
+#endif
 #include "ManusVRAnimInstance.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogManusVRAnimation, All, All);
+
+
+#if (ENGINE_MAJOR_VERSION >= 4) && (ENGINE_MINOR_VERSION >= 11)
+class UManusVRAnimInstance;
+
+struct FManusVRAnimInstanceProxy : public FAnimInstanceProxy
+{
+	FManusVRAnimInstanceProxy() : FAnimInstanceProxy(), ManusVRAnimInstance(nullptr) {}
+	FManusVRAnimInstanceProxy(UAnimInstance* Instance) : FAnimInstanceProxy(Instance), ManusVRAnimInstance(nullptr) {}
+
+	virtual bool Evaluate(FPoseContext& Output) override;
+
+	UManusVRAnimInstance*	ManusVRAnimInstance;
+};
+#endif
+
 
 UCLASS(Blueprintable, BlueprintType)
 class UManusVRAnimInstance : public UAnimInstance
 {
 	GENERATED_UCLASS_BODY()
 
+#if (ENGINE_MAJOR_VERSION >= 4) && (ENGINE_MINOR_VERSION >= 11)
+	bool ManusEvaluate(struct FPoseContext& Output);
+#else
 		bool NativeEvaluateAnimation(struct FPoseContext& Output) override;
+#endif
 
 	// Animation to use
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ManusVR)
@@ -38,4 +62,14 @@ class UManusVRAnimInstance : public UAnimInstance
 	UFUNCTION(BlueprintCallable, Category = ManusVR)
 		void GetLeftHandFingers(TArray<float>& OutFingers);
 
+
+#if (ENGINE_MAJOR_VERSION >= 4) && (ENGINE_MINOR_VERSION >= 11)
+private:
+	UPROPERTY(Transient, BlueprintReadOnly, Category = ManusVR, meta = (AllowPrivateAccess = "true"))
+		FManusVRAnimInstanceProxy Proxy;
+
+	virtual FAnimInstanceProxy* CreateAnimInstanceProxy() override { return& Proxy; }
+	virtual void DestroyAnimInstanceProxy(FAnimInstanceProxy* InProxy) override {}
+	friend struct FManusVRAnimInstanceProxy;
+#endif
 };
